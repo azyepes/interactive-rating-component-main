7b0b403e0815ee256d10f092588871e53b2101fa
Luis Yps Fdez <lueryefe@gmail.com>
2022-11-02 19:58:51 -0500

    Mobile version first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let $ = (id) => document.querySelector(id)
+
+let ratingSection = $('#rating-section')
+let thanksSection = $('#thanks-section')
+let button1 = $('#button1')
+let button2 = $('#button2')
+let button3 = $('#button3')
+let button4 = $('#button4')
+let button5 = $('#button5')
+let rate = $('#rate')
+
