54122fde078d342029f0381affe8f2f6b53ae9b3
Luis Yps Fdez <lueryefe@gmail.com>
2022-11-04 21:08:23 -0500

    Final review

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ let $ = (id) => document.querySelector(id)
 
 let ratingSection = $('#rating-section')
 let thanksSection = $('#thanks-section')
-let button1 = $('#button1')
-let button2 = $('#button2')
-let button3 = $('#button3')
-let button4 = $('#button4')
-let button5 = $('#button5')
-let rate = $('#rate')
+let rateSelected = $('#rate-selected')
+let input = document.getElementsByName('radio')
+let submit = $('#submit')
+let required = $('.required')
 
+submit.addEventListener('click', () => {
+
+    for (let i = 0; i < input.length; i++) {
+
+        if (input[i].checked) {
+            const element = input[i].value;
+
+            ratingSection.classList.add('hiden')
+            thanksSection.classList.remove('hiden')
+
+            rateSelected.textContent = element
+            break
+        } else {
+            required.textContent = 'Rate us to continue, please.'
+        }
+    }
+}
+)
